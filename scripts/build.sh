8a57f144d05abf3983a365377a4e388af1f57cf1
Emil Holm Gjørup <eg@concordium.com>
2021-02-11 13:52:16 +0100

    Disable release of main branch

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@
 set -e # Fail script on error
 
 # Branches to include in the build, must be separated by comma and no spaces.
-all_versions='testnet4,main'
+all_versions='testnet4'
 # Languages to include in the build
 all_languages='en'
 
