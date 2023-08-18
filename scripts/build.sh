917753a653f83b2e961d3522818bf9ab4a57a75f
Emil Holm Gjørup <eg@concordium.com>
2023-08-18 10:57:53 +0200

    Add concordium academy when deploying

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,45 +1,35 @@
 #!/bin/bash
 
-# Builds the different versions and translations of the documentation.
+# Build the developer documentation and the Concordium Academy.
 # This is assumed to be called from the project root.
 
 set -e # Fail script on error
 
-# Documentation projects to include in the build, must be separated by comma and no spaces.
-all_projects='mainnet,academy'
-
-# Languages to include in the build
-all_languages='en'
+printf "Building Concordium Developer Documentation... \n\n"
 
 source_dir='source'
 build_dir='build'
-
-# Expose settings for the build to create links between them
-export all_projects
-export all_languages
-
-# Convert the strings to arrays
-IFS="," read -a projects <<< $all_projects
-IFS="," read -a languages <<< $all_languages
-
-printf "Building documentation versions: ${all_projects} for languages ${all_languages}\n"
 
 mkdir -p "${build_dir}"
 
 printf "Copying 'public' to '${build_dir}'\n"
 cp -rv public/* ${build_dir}
 
-for current_project in ${projects[@]}; do
-  printf "\nProject '${current_project}':\n-----------------------------\n"
-  export current_version
+printf "\nRunning build 'mainnet'\n"
+sphinx-build "${source_dir}/mainnet" "${build_dir}/en/mainnet" -W
 
-  for current_language in ${languages[@]}; do
-    export current_language
-    printf "\nRunning build '${current_project}' for language '${current_language}'\n"
-    sphinx-build "${source_dir}/${current_project}" "${build_dir}/${current_language}/${current_project}" -D language="${current_language}" -W
-  done
+printf "Adding symlink ${build_dir}/404.html to en/mainnet/404.html\n"
+ln -sf "en/mainnet/404.html" "${build_dir}/404.html"
 
-printf "Adding symlink ${build_dir}/404.html to ${languages[0]}/${projects[0]}/404.html\n"
-ln -sf "${languages[0]}/${projects[0]}/404.html" "${build_dir}/404.html"
+printf "\nDone building Concordium Developer Documentation to '${build_dir}'\n"
 
-done
+
+# Build Concordium Academy, depends on objects.inv produce by mainnet for checking links.
+printf "\nBuilding Concordium Academy... \n\n"
+
+academy_build_dir='build-academy'
+
+mkdir -p "${academy_build_dir}"
+sphinx-build "${source_dir}/academy" "${academy_build_dir}" -W
+
+printf "Done building Concordium Academy to '${academy_build_dir}'\n"
