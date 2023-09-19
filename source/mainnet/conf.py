250e9ed5da4db883ca8752110c03749cf6cfce45
dg-concordium <dg@concordium.com>
2023-09-19 12:27:26 +0200

    Fix tippy

--- source/mainnet/conf.py
+++ source/mainnet/conf.py
@@ -226,10 +226,10 @@ html_context = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["../_static"]
-html_css_files = ["style.css","tippy.css"]
 
 html_css_files = [
     '../_static/css/style.css',
+    '../_static/css/tippy.css',
 ]
 
 # Custom sidebar templates, must be a dictionary that maps document names
