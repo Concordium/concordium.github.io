1c370dab9b3785733fa190b0ebce77ae9c188d50
dg-concordium <dg@concordium.com>
2024-02-06 08:40:19 +0100

    Edit conf.py

--- source/mainnet/conf.py
+++ source/mainnet/conf.py
@@ -157,8 +157,7 @@ html_theme_options = {
     "navbar_center": ["navbar-menu"],
     "navbar_end": ["navbar-icon-links"],
     "page_sidebar_items": ["page-toc", "edit-this-page", "footer-article"],
-    "footer_start": ["copyright"],
-    "footer_items": ["footer"],
+    "footer_items": ["copyright", "footer"],
     "show_prev_next": True,
     "navbar_align": "left",
     "icon_links": [
