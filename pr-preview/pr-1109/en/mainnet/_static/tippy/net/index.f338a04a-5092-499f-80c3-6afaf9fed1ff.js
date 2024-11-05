582d719e5c5118572966ec940185d0e0202d6ca0
chportma <chportma@users.noreply.github.com>
2024-11-05 14:57:35 +0000

    Deploy preview for PR 1109 🛫

---
+++ pr-preview/pr-1109/en/mainnet/_static/tippy/net/index.f338a04a-5092-499f-80c3-6afaf9fed1ff.js
@@ -0,0 +1,22 @@
+selector_to_html = {"a[href=\"resources/glossary.html\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">Glossary of Concordium Terms<a class=\"headerlink\" href=\"#glossary-of-concordium-terms\" title=\"Link to this heading\">#</a></h1><p>Also see the Concordium <a class=\"reference external\" href=\"https://developer.concordium.software/governance/whitepaper/Concordium%20White%20Paper.pdf\">Whitepaper</a> for more details on the terms described below.</p>"}
+skip_classes = ["headerlink", "sd-stretched-link"]
+
+window.onload = function () {
+    for (const [select, tip_html] of Object.entries(selector_to_html)) {
+        const links = document.querySelectorAll(`article.bd-article ${select}`);
+        for (const link of links) {
+            if (skip_classes.some(c => link.classList.contains(c))) {
+                continue;
+            }
+
+            tippy(link, {
+                content: tip_html,
+                allowHTML: true,
+                arrow: true,
+                placement: 'auto-start', maxWidth: 500, interactive: false,
+
+            });
+        };
+    };
+    console.log("tippy tips loaded!");
+};
