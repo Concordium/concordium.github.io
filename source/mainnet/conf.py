6db75073cd2d4b71751a7400dcc1d9faf7a26f81
Dragos-Gabriel Danciulescu <dragos-gabrieldanciulescu@MacBook-Pro-tilhrende-Dragos-Gabriel.local>
2024-12-10 12:39:03 +0200

    Light mode default for now, until feature is complete

--- source/mainnet/conf.py
+++ source/mainnet/conf.py
@@ -256,7 +256,7 @@ html_context = {
     "github_version": "main",
     "doc_path": "source/mainnet",
     "conf_py_path": "/source/",
-    "default_mode": "auto",
+    "default_mode": "light",
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
