5d9e01a980f7b30592d91e4aab1e20499fd9a403
TinaKT <TinaKT@users.noreply.github.com>
2025-09-05 11:15:07 +0000

    Deploy preview for PR 1416 🛫

---
+++ pr-preview/pr-1416/en/mainnet/_static/tippy/docs/protocol/identity-disclosure-processes.24fb7d44-2674-4518-b768-62f421004651.js
@@ -0,0 +1,22 @@
+selector_to_html = {"a[href=\"../resources/glossary.html#term-Linking-key\"]": "<dt id=\"term-Linking-key\">Linking key</dt><dd><p>The linking key allows to find all accounts of a given account holder. This identifier is stored in encrypted form in the identity record of an account holder.</p></dd>"}
+skip_classes = ["headerlink", "sd-stretched-link"]
+
+window.onload = function () {
+    for (const [select, tip_html] of Object.entries(selector_to_html)) {
+        const links = document.querySelectorAll(`article.bd-article ${select}`);
+        for (const link of links) {
+            if (skip_classes.some(c => link.classList.contains(c))) {
+                continue;
+            }
+
+            tippy(link, {
+                content: tip_html,
+                allowHTML: true,
+                arrow: true,
+                placement: 'auto-start', maxWidth: 500, interactive: false,
+
+            });
+        };
+    };
+    console.log("tippy tips loaded!");
+};
