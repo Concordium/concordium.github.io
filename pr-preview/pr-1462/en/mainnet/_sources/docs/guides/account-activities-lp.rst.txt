22c8fe8fc020a190a87d26851b7f0975a0852aa4
TinaKT <TinaKT@users.noreply.github.com>
2025-12-11 15:10:45 +0000

    Deploy preview for PR 1462 🛫

---
+++ pr-preview/pr-1462/en/mainnet/_sources/docs/guides/account-activities-lp.rst.txt
@@ -0,0 +1,24 @@
+.. _account-activities-lp:
+.. include:: ../../variables.rst
+
+==================
+Account activities
+==================
+
+If you need to know how to send CCDs, connect dApps, manage fungible and non-fungible tokens, or more, all the information you need is right here.
+
+.. toctree::
+    :hidden:
+    :maxdepth: 2
+
+    send-ccd-wallets
+    ../mobile-wallet/share-address-mw
+    ../browser-wallet/connect-app
+    tokens
+    shield-ccd-wallets
+    scheduled-release
+    export-account-private-key
+    export-transaction-logs
+    add-delegation
+    remove-delegation
+    update-delegation
---
+++ pr-preview/pr-1462/en/mainnet/_static/tippy/docs/network/guides/run-local-chain.bcab8bc2-3692-4fef-b391-4f2f6ecf8c23.js
@@ -0,0 +1,22 @@
+selector_to_html = {"a[href=\"../../resources/glossary.html#term-Mainnet\"]": "<dt id=\"term-Mainnet\">Mainnet</dt><dd><p>The main Concordium network which launched in June 2021. The mainnet will receive periodic upgrades, but in contrast to the <a class=\"reference internal\" href=\"#term-Testnet\"><span class=\"xref std std-term\">testnet</span></a>, it will never be reset, and accounts created on the mainnet will remain indefinitely.</p></dd>", "a[href=\"../../resources/glossary.html#term-Testnet\"]": "<dt id=\"term-Testnet\">Testnet</dt><dd><p>A test network run by Concordium to test its protocols and software. There can be several test networks in existence at the same time. All the features are tested on the testnet before they are released on the <a class=\"reference internal\" href=\"#term-Mainnet\"><span class=\"xref std std-term\">mainnet</span></a>.</p></dd>", "a[href=\"../../resources/glossary.html#term-Genesis-Block\"]": "<dt id=\"term-Genesis-Block\">Genesis Block</dt><dd><p>The first <a class=\"reference internal\" href=\"#term-Block\"><span class=\"xref std std-term\">block</span></a> in a <a class=\"reference internal\" href=\"#term-Chain\"><span class=\"xref std std-term\">chain</span></a>. The genesis block establishes the starting state of the chain, before any transactions have occurred.</p></dd>", "a[href=\"../../resources/glossary.html#term-Privacy-guardian-PG\"]": "<dt id=\"term-Privacy-guardian-PG\">Privacy guardian (PG)</dt><dd><p>Authorized entity participating in Concordium\u2019s identity disclosure process when legally required. PGs hold cryptographic keys that enable them to decrypt partial shares of encrypted identity information. Multiple PGs must collaborate to reconstruct complete identity data - a minimum threshold (minimum two out of three) of PGs must provide their decryption shares before the Authority can access the full information needed to connect accounts to identities.</p></dd>"}
+skip_classes = ["headerlink", "sd-stretched-link"]
+
+window.onload = function () {
+    for (const [select, tip_html] of Object.entries(selector_to_html)) {
+        const links = document.querySelectorAll(`article.bd-article ${select}`);
+        for (const link of links) {
+            if (skip_classes.some(c => link.classList.contains(c))) {
+                continue;
+            }
+
+            tippy(link, {
+                content: tip_html,
+                allowHTML: true,
+                arrow: true,
+                placement: 'auto-start', maxWidth: 500, interactive: false,
+
+            });
+        };
+    };
+    console.log("tippy tips loaded!");
+};
